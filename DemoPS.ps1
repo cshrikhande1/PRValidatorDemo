42dad4e00d86f3d187ee45cd919024158e2d727a
cshrikhande1 <chshrikh1@hotmail.com>
2016-04-21 17:21:15 +0530

    chshrikh1 doing change

--- DemoPS.ps1
+++ DemoPS.ps1
@@ -4,3 +4,6 @@ write-host "Hello from my demo ps1"
 ## Contribution from user chshrikh1
 
 write-host "Hello VSTS task world"
+
+This is from chshrikh1 which is form out side of contributor list of this repo 
+Thanks 
